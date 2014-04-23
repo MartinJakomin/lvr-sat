aa82002254d36bc40c2db904aa7774d95d76f36c
Martin Jakomin <martin.jakomin@gmail.com>
2014-04-23 20:56:18 +0200

    Added final versions of the files

---
+++ SAT/bool.py
@@ -0,0 +1,276 @@
+__author__ = "Martin Jakomin, Mateja Rojko"
+
+
+"""
+Boolean operators:
+- Variable
+- Negation
+- Or
+- And
+- Constant
+
+Functions:
+- solve
+- solve_cnf
+- nnf
+- cnf
+"""
+
+import itertools
+
+
+# functions
+
+
+def nnf(f):
+    """ Returns negation normal form """
+    return f.nnf()
+
+
+def simplify(f):
+    """ Simplifies the expression """
+    return nnf(f).simplify()
+
+
+def cnf(f):
+    """ Returns conjunctive normal form """
+    return nnf(f).cnf().simplify()
+
+
+def solve(f, v):
+    """ Solves the expression using the variable values v """
+    return f.solve(v)
+
+
+def solve_cnf(f, v):
+    """ Simplifies the cnf form using the variable values v """
+    return cnf(f.solve_cnf(v))
+
+
+# classes
+
+
+class Var():
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+    def solve(self, v):
+        return v[self.name]
+
+    def solve_cnf(self, v):
+        if self.name in v:
+            return Const(v[self.name])
+        else:
+            return self
+
+    def nnf(self):
+        return self
+
+    def simplify(self):
+        return self
+
+    def cnf(self):
+        return self
+
+    def length(self):
+        return 1
+
+
+class Neg():
+    def __init__(self,v):
+        self.value = v
+
+    def __str__(self):
+        return "~" + str(self.value.__str__())
+
+    def solve(self, v):
+        return not(self.value.solve(v))
+
+    def solve_cnf(self, v):
+        if self.value.name in v:
+            return Const(not(v[self.value.name]))
+        else:
+            return self
+
+    def nnf(self):
+        v = self.value
+        if isinstance(v, Var):
+            return Neg(v)
+        elif isinstance(v, Neg):
+            return v.value.nnf()
+        elif isinstance(v, And):
+            return Or([Neg(x) for x in v.value]).nnf()
+        elif isinstance(v, Or):
+            return And([Neg(x) for x in v.value]).nnf()
+        elif isinstance(v, Const):
+            return v.negate()
+
+    def simplify(self):
+        return self
+
+    def cnf(self):
+        return self
+
+    def length(self):
+        return self.value.length()
+
+
+class And():
+    def __init__(self,lst):
+        self.value = lst
+
+    def __str__(self):
+        s = "("
+        for i in self.value:
+            s += str(i)+" & "
+        s = s[:len(s)-3]
+        return s + ")"
+
+    def solve(self, v):
+        for l in self.value:
+            if l.solve(v) is False:
+                return False
+        return True
+
+    def solve_cnf(self, v):
+        return And([x.solve_cnf(v) for x in self.value])
+
+    def nnf(self):
+        return And([x.nnf() for x in self.value])
+
+    def simplify(self):
+        s = [x.simplify() for x in self.value]
+        # And list flatten
+        ns = []
+        for x in s:
+            if isinstance(x, And):
+                ns.extend(x.value)
+            else:
+                ns.append(x)
+        s = ns
+        snames = [x.simplify().__str__() for x in s]
+        s2 = []
+        for i, x in enumerate(s):
+            if Neg(x).nnf().__str__() in snames[i+1:]:
+                return Const(False)
+            elif isinstance(x, Const):
+                if x.value is False:
+                    return Const(False)
+            elif snames[i] not in snames[i+1:]:
+                s2.append(x)
+
+        if len(s2) < 1:
+            return Const(True)
+        elif len(s2) is 1:
+            return s2[0]
+        return And(s2)
+
+    def cnf(self):
+        return And([x.cnf().simplify() for x in self.value])
+
+    def length(self):
+        return sum([x.length() for x in self.value])
+
+
+class Or():
+    def __init__(self, lst):
+        self.value = lst
+
+    def __str__(self):
+        s = "("
+        for i in self.value:
+            s += str(i)+" | "
+        s = s[:len(s)-3]
+        return s + ")"
+
+    def solve(self, v):
+        for l in self.value:
+            if l.solve(v) is True:
+                return True
+        return False
+
+    def solve_cnf(self, v):
+        return Or([x.solve_cnf(v) for x in self.value])
+
+    def nnf(self):
+        return Or([x.nnf() for x in self.value])
+
+    def simplify(self):
+        s = [x.simplify() for x in self.value]
+        # Or list flatten
+        ns = []
+        for x in s:
+            if isinstance(x,Or):
+                ns.extend(x.value)
+            else:
+                ns.append(x)
+        s = ns
+        snames = [x.simplify().__str__() for x in s]
+        s2 = []
+        for i, x in enumerate(s):
+            if Neg(x).nnf().__str__() in snames[i+1:]:
+                return Const(True)
+            elif isinstance(x, Const):
+                if x.value is True:
+                    return Const(True)
+            elif snames[i] not in snames[i+1:]:
+                s2.append(x)
+
+        if len(s2) < 1:
+            return Const(False)
+        elif len(s2) is 1:
+            return s2[0]
+        return Or(s2)
+
+    def cnf(self):
+        s = [x.cnf().simplify() for x in self.value]
+        s1 = [x.value if isinstance(x, And) else [x] for x in s]
+        s2 = []
+        for e in itertools.product(*s1):
+            s3 = []
+            for x in e:
+                if isinstance(x,Or):
+                    s3.extend(x.value)
+                else:
+                    s3.append(x)
+            s2.append(Or(s3))
+        if len(s2) is 1:
+            return s2[0]
+        return And(s2)
+
+    def length(self):
+        return sum([x.length() for x in self.value])
+
+
+class Const():
+    def __init__(self, c):
+        self.value = c
+
+    def __str__(self):
+        return str(self.value)
+
+    def solve(self, v):
+        return self.value
+
+    def solve_cnf(self, v):
+        return self
+
+    def nnf(self):
+        return self
+
+    def negate(self):
+        if self.value is True:
+            return Const(False)
+        return Const(True)
+
+    def simplify(self):
+        return self
+
+    def cnf(self):
+        return self
+
+    def length(self):
+        return 1
---
+++ SAT/sat.py
@@ -0,0 +1,121 @@
+__author__ = "Martin Jakomin, Mateja Rojko"
+
+
+"""
+SAT solver based on DPLL algorithm
+
+Functions:
+- get_literals_dict
+- sat
+"""
+
+from bool import Var, Neg, Const, cnf, solve_cnf
+
+# functions
+
+
+def get_literals_dict(f):
+    """
+    Gets a dictionary of all literals with information about their purity and independence
+    """
+
+    s = []
+    ind = []
+    r = {}
+
+    # If f is literal return it
+    if isinstance(f, Var):
+        r[f.name] = 1
+        return r
+    elif isinstance(f, Neg):
+        r[f.value.name] = -1
+        return r
+
+    # Flattens the list
+    for x in f.value:
+        if isinstance(x, Or) or isinstance(x, And):
+            s.extend(x.value)
+        else:
+            s.append(x)
+            ind.append(x)
+    # Purity check
+    for x in s:
+        if isinstance(x, Var):
+            if x.name in r and r[x.name] is not 1:
+                r[x.name] = 0
+            else:
+                r[x.name] = 1
+        elif isinstance(x, Neg):
+            if x.value.name in r and r[x.value.name] is not -1:
+                r[x.value.name] = 0
+            else:
+                r[x.value.name] = -1
+    # Independent variables
+    for x in ind:
+        if isinstance(x, Var):
+            r[x.name] = 1
+        elif isinstance(x, Neg):
+            r[x.value.name] = -1
+    return r
+
+
+def sat(f,v):
+    """
+    Advanced DPLL algorithm, with while loop simplification and clause length sorting
+    """
+
+    f = cnf(f)
+    # If f is simplified to a constant
+    if isinstance(f, Const):
+        if f.value is True:
+            return True, v
+        else:
+            return False, {}
+
+    # While loop simplify
+    xs = get_literals_dict(f)
+    i = 0
+    while 1 in xs.values() or -1 in xs.values():
+        # Deleting of pure literals
+        for x in xs.keys():
+            if xs[x] is 1:
+                v[x] = True
+            elif xs[x] is -1:
+                v[x] = False
+        f = solve_cnf(f, v)
+        # If f is simplified to a constant
+        if isinstance(f, Const):
+            if f.value is True:
+                return True, v
+            else:
+                return False, {}
+        xs = get_literals_dict(f)
+        i += 1
+
+    # Clauses sorted by length
+    f2 = sorted(f.value, key=lambda x: x.length())
+    first = f2[0]
+    first = first.value[0]
+    # Get the name of first literal in shortest clause
+    if isinstance(first, Var):
+        first = first.name
+    else:
+        first = first.value.name
+
+    # Safe copy of dictionary
+    v2 = v.copy()
+
+    # Case true
+    v[first] = True
+    ft = solve_cnf(f, v)
+    rec = sat(ft, v)
+    if rec[0]:
+        return rec
+    # Case false
+    else:
+        v2[first] = False
+        ff = solve_cnf(f, v2)
+        rec2 = sat(ff, v2)
+        return rec2
+
+# no classes
